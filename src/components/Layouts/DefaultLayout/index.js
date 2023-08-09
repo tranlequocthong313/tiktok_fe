2364bcacfc1148a964beaeb1de6fe2ef2fdf8df8
tranlequocthong313 <tranlequocthong313@gmail.com>
2023-08-09 20:27:33 +0700

    updated default layout's components

--- src/components/Layouts/DefaultLayout/index.js
+++ src/components/Layouts/DefaultLayout/index.js
@@ -1,13 +1,17 @@
 import Header from '~/components/Layouts/common/Header';
 import Sidebar from './Sidebar';
+import styles from './DefaultLayout.module.scss';
+import classNames from 'classnames/bind';
 
 function DefaultLayout({ children }) {
+    const cx = classNames.bind(styles);
+
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <Sidebar />
-                <div className="content">{children}</div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
